2780afab746102270aaf878cb8f852880d6b2063
TSRStormed <107441806+TSRStormed@users.noreply.github.com>
2026-01-27 20:26:23 -0700

    Reorder function call in __overlay_lair_release

--- src/instant_ffm_100_ffm.c
+++ src/instant_ffm_100_ffm.c
@@ -5,10 +5,10 @@
 #include "recompconfig.h"
 #include "enums.h"
 
+extern struct FF_StorageStruct *D_8037DCB8;
+
 // Patches a function in the base game that enables 100% FFM moves once entering MM
 RECOMP_PATCH void __overlay_lair_release(void){
-    lair_func_8038CD48();
-
     ability_setLearned(ABILITY_1_BEAK_BOMB);
     ability_setLearned(ABILITY_2_BEAK_BUSTER);
     ability_setLearned(ABILITY_D_SHOCK_JUMP);
@@ -16,4 +16,6 @@ RECOMP_PATCH void __overlay_lair_release(void){
     ability_setLearned(ABILITY_10_TALON_TROT);
     ability_setLearned(ABILITY_11_TURBO_TALON);
     ability_setLearned(ABILITY_13_1ST_NOTEDOOR);
+
+    lair_func_8038CD48();
 }
