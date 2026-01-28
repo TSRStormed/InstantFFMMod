497f4c0e8d8ebd4f9419c92297fc4e1fe49b5f67
TSRStormed <107441806+TSRStormed@users.noreply.github.com>
2026-01-27 20:26:53 -0700

    Reorganize __overlay_lair_release function calls
    Reordered the function calls in __overlay_lair_release to set abilities before calling lair_func_8038CD48.

--- src/instant_ffm_any.c
+++ src/instant_ffm_any.c
@@ -7,8 +7,7 @@
 
 // Patches a function in the base game that enables Any% FFM moves once entering MM
 RECOMP_PATCH void __overlay_lair_release(void){
-    lair_func_8038CD48();
-
     ability_setAllLearned(-1);
+    
+    lair_func_8038CD48();
 }
-
